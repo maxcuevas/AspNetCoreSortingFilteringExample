8ea19002f98792d11d7dde6f303b34aef442fc8a
Max Cuevas <maximilianocuevas777@gmail.com>
2019-07-21 08:48:40 -0700

    First commit

---
+++ AspNetCoreSortingFilterExample/Models/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreSortingFilterExample.Models
+{
+    public partial class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
